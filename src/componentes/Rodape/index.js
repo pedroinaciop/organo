0445cf1ba8289f125d8cc523b0719dee7886f3d0
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2024-03-29 18:15:41 -0300

    rodape

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,23 +1,35 @@
-import "./Rodape.css";
+import './Rodape.css'
 
 const Rodape = () => {
     return (
-        <footer className="rodape">
-            <div>
-                <a href="facebook.com" target="_blank">
-                    <img className="redes-sociais" src="/images/fb.png" alt="Ícone do facebook"/>
-                </a>
-                <a href="instagram.com" target="_blank">
-                    <img className="redes-sociais" src="/images/tw.png" alt="Ícone do twitter"/>
-                </a>
-                <a href="instagram.com" target="_blank">
-                    <img className="redes-sociais" src="/images/ig.png" alt="Ícone do instagram"/>
-                </a>
-            </div>
-            <img src="/images/logo.png" alt="Logo do Organo"/>
+    <footer className="footer">
+        <section>
+            <ul>
+                <li>
+                    <a href="facebook.com" target="_blank">
+                        <img src="/images/fb.png" alt="ícone do Facebook" />
+                    </a>
+                </li>
+                <li>
+                    <a href="twitter.com" target="_blank">
+                        <img src="/images/tw.png" alt="ícone do Twitter" />
+                    </a>
+                </li>
+                <li>
+                    <a href="instagram.com" target="_blank">
+                        <img src="/images/ig.png" alt="ícone do Instagram" />
+                    </a>
+                </li>
+            </ul>
+        </section>
+        
+        <img src="/images/logo.png" alt="Logo do Organo" />
+        
+        <section>
             <p>Desenvolvido por pedroinaciop</p>
-        </footer>
-    ) 
+        </section>
+    </footer>
+    )
 }
 
-export default Rodape;
+export default Rodape
