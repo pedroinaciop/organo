a07a4c0c245de67b265b4ff86c3bda4f79d1e1b5
Pedro Inácio Penha dos Santos <inaciopedro2004@gmail.com>
2024-04-01 18:17:35 -0300

    Importando biblioteca uuid

--- src/App.js
+++ src/App.js
@@ -4,34 +4,42 @@ import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
 import Rodape from "./componentes/Rodape";
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: "Programação",
       cor: "#D9F7E9",
     },
     {
+      id: uuidv4(),
       nome: "Front-End",
       cor: "#E8F8FF",
     },
     {
+      id: uuidv4(),
       nome: "Data-Science",
       cor: "#F0F8E2",
     },
     {
+      id: uuidv4(),
       nome: "Devops",
       cor: "#FDE7E8",
     },
     {
+      id: uuidv4(),
       nome: "UX e Design",
       cor: "#FAE9F5",
     },
     {
+      id: uuidv4(),
       nome: "Mobile",
       cor: "#FFF5D9",
     },
     {
+      id: uuidv4(),
       nome: "Inovação e Gestão",
       cor: "#FFEEDF",
     },
@@ -40,7 +48,6 @@ function App() {
   const [colaboradores, setColaborador] = useState([]);
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
-    // debugger
     setColaborador([...colaboradores, colaborador]);
   };
 
